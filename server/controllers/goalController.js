27a8777cda188b518f458756e57908c00641d77f
poyanpayandeh <87450102+poyanpayandeh1@users.noreply.github.com>
2022-05-23 21:46:01 +1200

    error handing and async set up

--- server/controllers/goalController.js
+++ server/controllers/goalController.js
@@ -1,30 +1,36 @@
+const asyncHandler = require("express-async-handler");
+
 // get goals
 // @route GET /api/goals
 // @access Private
-const getGoals = (req, res) => {
+const getGoals = asyncHandler(async (req, res) => {
   res.json({ message: "get goals" });
-};
+});
 
 // create goal
 // @route POST /api/goals
 // @access Private
-const setGoal = (req, res) => {
+const setGoal = asyncHandler(async (req, res) => {
+  if (!req.body.message) {
+    res.status(400);
+    throw new Error("fu");
+  }
   res.json({ mesasge: "set goals" });
-};
+});
 
 // get goals
 // @route PUT /api/goals/id:
 // @access Private
-const updateGoal = (req, res) => {
+const updateGoal = asyncHandler(async (req, res) => {
   res.json({ mesasge: `update goal ${req.params.id}` });
-};
+});
 
 // delete goals
 // @route DELETE /api/goals/id:
 // @access Private
-const deleteGoal = (req, res) => {
+const deleteGoal = asyncHandler(async (req, res) => {
   res.json({ mesasge: `delete goal ${req.params.id}` });
-};
+});
 
 module.exports = {
   getGoals,
--- server/server.js
+++ server/server.js
@@ -2,10 +2,14 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 3002;
+const { errorHandler } = require("./middleware/errorMiddleware");
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extented: false }));
 
 // routes
 app.use("/api/goals", require("./routes/goalRoutes"));
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server running on ${port}`));
