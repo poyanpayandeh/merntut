0eace8fa1c57820142dac8ee264d58065311fbc4
poyanpayandeh <87450102+poyanpayandeh1@users.noreply.github.com>
2022-05-23 21:03:30 +1200

    y

--- server/routes/goalRoutes.js
+++ server/routes/goalRoutes.js
@@ -7,16 +7,9 @@ const {
   deleteGoal,
 } = require("../controllers/goalController");
 
-// get goal
 router.get("/", getGoals);
-
-// create goal
 router.post("/", setGoal);
-
-// update goal
 router.put("/:id", updateGoal);
-
-//delete goal
 router.delete("/:id", deleteGoal);
 
 module.exports = router;
