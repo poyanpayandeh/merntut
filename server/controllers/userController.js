1071f6b0295cd4e8c8a9a71b3bc5260676e6a2ac
poyanpayandeh <87450102+poyanpayandeh1@users.noreply.github.com>
2022-05-29 18:28:15 +1200

    jwt token set up

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -38,6 +38,7 @@ const registerUser = asyncHandler(async (req, res) => {
       _id: user.id,
       name: user.name,
       email: user.email,
+      token: generateJWT(user._id),
     });
   } else {
     res.status(400);
@@ -61,6 +62,7 @@ const loginUser = asyncHandler(async (req, res) => {
       _id: user.id,
       name: user.name,
       email: user.email,
+      token: generateJWT(user._id),
     });
   } else {
     res.status(400);
@@ -74,6 +76,11 @@ const loginUser = asyncHandler(async (req, res) => {
 const getMe = asyncHandler(async (req, res) => {
   res.json({ message: "User Data" });
 });
+
+// Gererate JWT
+const generateJWT = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30d" });
+};
 
 module.exports = {
   registerUser,
